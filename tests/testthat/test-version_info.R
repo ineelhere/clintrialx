85b2aacdca47235a46eb48e0afb0338f3c5c24d0
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2024-07-23 23:38:51 +0530

    [UPDATE] test expect for version_info func

--- tests/testthat/test-version_info.R
+++ tests/testthat/test-version_info.R
@@ -2,6 +2,6 @@ library(testthat)
 library(clintrialx)
 
 test_that("version_info works correctly", {
-  expect_message(version_info(), "Clinicaltrials.gov API version")
+  expect_message(version_info("aact"), "Please refer to: https://aact.ctti-clinicaltrials.org/api/v2/version")
   expect_error(version_info("unsupported_source"), "Unsupported source")
 })
